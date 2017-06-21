ea3120aeb5a9d858d6f1d023c53e90e36ca65a49
Mattia Oriani <mattiaoriani@gmail.com>
2017-06-21 21:26:44 +0200

    risistemati bordi

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -6,7 +6,7 @@ import {FlashMessagesService} from 'angular2-flash-messages';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   username: String;
