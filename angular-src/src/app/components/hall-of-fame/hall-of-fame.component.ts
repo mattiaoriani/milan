51b6506d185eea6aff0cec62dbe39957b71c6c01
Mattia Oriani <mattiaoriani@gmail.com>
2017-06-24 17:56:07 +0200

    fatto halloffame e singolo calciatore

--- angular-src/src/app/components/hall-of-fame/hall-of-fame.component.ts
+++ angular-src/src/app/components/hall-of-fame/hall-of-fame.component.ts
@@ -31,42 +31,10 @@ export class HallOfFameComponent implements OnInit {
     });
   }
 
-  getSumAppearences(footballer) {
-    let total = 0;
-    for(var i = 0; i < footballer["statics"].length; i++){
-        total +=footballer["statics"][i]["apperances"];
-    }
-    return total;
-  }
+  getPlayerCoverImg(footballer){
+    console.log(footballer.coverImg);
+    return footballer.coverImg;
 
-  getSumGoals(footballer) {
-    let total = 0;
-    for(var i = 0; i < footballer["statics"].length; i++){
-        total +=footballer["statics"][i]["goals"];
-    }
-    return total;
-  }
-
-  getThrophyPath(trophy) {
-    switch(trophy.name){
-      case "Scudetto Serie A": return "../../assets/hall-of-fame/scudetto.png";
-      case "Champions League": return "../../assets/hall-of-fame/championsleague.png";
-      case "Coppa Italia TIM": return "../../assets/hall-of-fame/coppaitalia.png";
-      case "UEFA Supercup": return "../../assets/hall-of-fame/supercoppaeuropea.png";
-      case "FIFA Club's World Cup": return "../../assets/hall-of-fame/fifaworldcup.png";
-      case "Supercoppa Italiana": return "../../assets/hall-of-fame/supercoppaitaliana.png";
-      case "Coppa Intercontinentale": return "../../assets/hall-of-fame/intercontinentale.png";
-      case "FIFA Pallone d'oro": return "../../assets/hall-of-fame/pallonedoro.png";
-      default : return "../../assets/hall-of-fame/presenze.png";;
-    }
-  }
-  checkIfHasThrophy(footballer){
-    var size = Object.keys(footballer["trophiesWon"]).length;
-    if(size==0){
-      return false;
-    }else{
-      return true;
-    }
   }
 
   getFilter(footballer){
