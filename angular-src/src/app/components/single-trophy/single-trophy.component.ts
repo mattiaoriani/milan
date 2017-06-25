85c88abcc3ef02759188dc76d9929618a90d1147
Mattia Oriani <mattiaoriani@gmail.com>
2017-06-25 11:50:22 +0200

    foto trofei

--- angular-src/src/app/components/single-trophy/single-trophy.component.ts
+++ angular-src/src/app/components/single-trophy/single-trophy.component.ts
@@ -9,8 +9,24 @@ import {ActivatedRoute } from '@angular/router';
 export class SingleTrophyComponent implements OnInit {
 
   trophy:Object;
-  
+
   constructor(private router:ActivatedRoute) { }
+
+  config: Object = {
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      spaceBetween: 0
+  };
+
+  photoconfig:Object = {
+      pagination: '.swiper-pagination-img',
+      slidesPerView: 1,
+      paginationClickable: true,
+      spaceBetween: 10,
+      freeMode: true
+  }
 
   ngOnInit() {
     this.router.params.subscribe(params => this.trophy = JSON.parse(params["id"]));
