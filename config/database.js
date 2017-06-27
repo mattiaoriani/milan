c6e55458555f9e6805f2b9fc27c18077fa2977d7
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-06-27 10:20:33 +0200

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/CasaMilanDB', 
+  //database: 'mongodb://localhost:27017/CasaMilanDB',
+  database: 'mongodb://federicovitali:wilmilan@ds139362.mlab.com:39362/casamilandb',
   secret: 'yoursecret'
 }
